1ab18b3d2d154a50f5c969059ef72c7a28c114af
Yohan-Baechle <baechle.yohan@gmail.com>
2024-09-18 09:32:54 +0200

    design général du scaffold terminé

--- lib/main.dart
+++ lib/main.dart
@@ -7,46 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AFPA - Formations CDA',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Accueil'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +30,186 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedBottomBarIndex = 0;
 
-  void _incrementCounter() {
+  final Color _selectedColor = Colors.black87;  // Couleur des éléments sélectionnés
+  final Color _unselectedColor = Colors.grey; // Couleur des éléments non sélectionnés
+  final Color _sliderColor = Colors.lightGreen;  // Couleur du slider (barre d'indication)
+  final Duration _animationDuration = const Duration(milliseconds: 300);
+
+  // Les pages associées à chaque élément du Drawer
+  final List<Widget> _pages = [
+    const Center(child: Text('Page d\'Accueil')),
+    const Center(child: Text('Formations')),
+    const Center(child: Text('Liens Utiles')),
+    const Center(child: Text('À propos de l\'application CDA')),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedBottomBarIndex = index;
     });
+  }
+
+  void _navigateToPage(int index) {
+    setState(() {
+      _selectedBottomBarIndex = index;
+    });
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              height: 48,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ],
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: const Text('John Doe'),
+              accountEmail: const Text('john.doe@example.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.network(
+                    'https://randomuser.me/api/portraits/men/46.jpg',
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.lightGreen,
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Accueil'),
+              selected: _selectedBottomBarIndex == 0,
+              selectedTileColor: Colors.lightGreenAccent.withOpacity(0.2),
+              onTap: () => _navigateToPage(0),
+            ),
+            ListTile(
+              leading: const Icon(Icons.school),
+              title: const Text('Formations'),
+              selected: _selectedBottomBarIndex == 1,
+              selectedTileColor: Colors.lightGreenAccent.withOpacity(0.2),
+              onTap: () => _navigateToPage(1),
+            ),
+            ListTile(
+              leading: const Icon(Icons.link),
+              title: const Text('Liens Utiles'),
+              selected: _selectedBottomBarIndex == 2,
+              selectedTileColor: Colors.lightGreenAccent.withOpacity(0.2),
+              onTap: () => _navigateToPage(2),
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('À propos'),
+              selected: _selectedBottomBarIndex == 3,
+              selectedTileColor: Colors.lightGreenAccent.withOpacity(0.2),
+              onTap: () => _navigateToPage(3),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Déconnexion'),
+              selected: _selectedBottomBarIndex == 4,
+              selectedTileColor: Colors.lightGreenAccent.withOpacity(0.3),
+              onTap: () {
+                Navigator.pop(context);
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Déconnexion"),
+                      content: const Text("Voulez-vous vraiment vous déconnecter ?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Non"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Oui"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: IndexedStack(
+        index: _selectedBottomBarIndex,
+        children: _pages, // Affiche la page correspondant à l'index sélectionné
+      ),
+      bottomNavigationBar: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+              ),
+            ),
+            child: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Accueil',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'Formations',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.link),
+                  label: 'Liens Utiles',
+                ),
+              ],
+              currentIndex: _selectedBottomBarIndex,
+              selectedItemColor: _selectedColor,  // Couleur personnalisée pour les éléments sélectionnés
+              unselectedItemColor: _unselectedColor,  // Couleur pour les éléments non sélectionnés
+              onTap: _onItemTapped,
+            ),
+          ),
+          AnimatedPositioned(
+            duration: _animationDuration,
+            curve: Curves.easeInOut, // Animation douce
+            left: MediaQuery.of(context).size.width / 3 * _selectedBottomBarIndex,
+            child: Container(
+              height: 4,
+              width: MediaQuery.of(context).size.width / 3,
+              color: _sliderColor,  // Conserve la couleur du slider en light green
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
